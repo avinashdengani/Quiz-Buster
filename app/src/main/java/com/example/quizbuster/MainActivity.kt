3501783dae48ccbf33843dcddad889135afc5e6b
Avinash Dengani <avinashdengani123@gmail.com>
2021-03-20 02:41:04 +0530

    Completed Welcome and User Name input Page

---
+++ app/src/main/java/com/example/quizbuster/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.quizbuster
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    public fun onSubmitName(view: View){
+
+    }
+}
